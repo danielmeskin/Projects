6ed36cc3f90b1eb073aa8d2e8d02c7af5ca9eb07
Daniel Meskin <github@dmeskin.xyz>
2021-04-06 19:21:28 -0400

    fix credit card check

--- Numbers/Rust/credit-card-validator/src/main.rs
+++ Numbers/Rust/credit-card-validator/src/main.rs
@@ -5,11 +5,15 @@ fn validate(mut card_number: String) -> bool {
     let check_digit=check_digit.expect("Failed to extract check digit.").to_digit(10).unwrap();
     let card_list=card_number.chars();
     let mut check_list=vec![];
-    for number in card_list.step_by(2) {
+    for number in card_list.clone().step_by(2) {
         let mut number=number.to_digit(10).unwrap()*2;
         if number >= 10 {
             number = (number-10)+1;
         }
+        check_list.push(number);
+    }
+    for number in card_list.step_by(2).skip(1) {
+        let number=number.to_digit(10).unwrap()*2;
         check_list.push(number);
     }
     let mut check_sum = 0;
